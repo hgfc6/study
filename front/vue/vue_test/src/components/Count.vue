46e57a9b6a0150113e1583ef55e7fdd9274b5913
cjh <jinghuicao21@gmail.com>
2025-09-23 16:47:51 +0800

    vue vuex求和案例(mapState,mapGetters,mapActions,mapMutations)

--- front/vue/vue_test/src/components/Count.vue
+++ front/vue/vue_test/src/components/Count.vue
@@ -1,51 +1,70 @@
 <template>
-	<div>
-		<h1>当前求和为：{{$store.state.sum}}</h1>
-		<h1>当前和乘10为：{{$store.getters.bigSum}}</h1>
-		<select v-model.number="n">
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-		</select>
-		<button @click="increment">+</button>
-		<button @click="decrement">-</button>
-		<button @click="incrementOdd">当前求和为奇数再加</button>
-		<button @click="incrementWait">等一等再加</button>
-	</div>
+  <div>
+    <h1>当前求和为：{{ $store.state.sum }}</h1>
+    <h2>当前和乘10为：{{ $store.getters.bigSum }}</h2>
+    <h2>当前和乘10为：{{ bigSum }}</h2><!--借助mapGetter实现-->
+    <h2>我来自{{ school }}，在学习{{ subject }}</h2><!--借助mapState实现-->
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increment">+</button>
+    <button @click="mapIncrement(n)">map+</button>
+    <button @click="decrement">-</button>
+    <button @click="mapDecrement(n)">map-</button><!--通过mapMutations生成的方法如果不传参的话，默认会把event作为参数传递过去，所以一般要写参数-->
+    <button @click="incrementOdd">当前求和为奇数再加</button>
+    <button @click="mapIncrementOdd(n)">map当前求和为奇数再加</button>
+    <button @click="incrementWait">等一等再加</button>
+    <button @click="mapIncrementWait(n)">map等一等再加</button>
+  </div>
 </template>
 
 <script>
-	export default {
-		name:'Count',
-		data() {
-			return {
-				n:1, //用户选择的数字
-			}
-		},
-		methods: {
-			increment(){
-				// this.$store.dispatch('jia',this.n) //因为action中的jia没有做任何附加处理，所以可以直接调用mutations中的JIA处理数据
-				this.$store.commit('JIA',this.n)
-			},
-			decrement(){
-				// this.$store.dispatch('jian',this.n)
-				this.$store.commit('JIAN',this.n)
-			},
-			incrementOdd(){
-				this.$store.dispatch('jiaOdd',this.n)
-			},
-			incrementWait(){
-				this.$store.dispatch('jiaWait',this.n)
-			},
-		},
-		mounted() {
-			console.log('Count',this)
-		},
-	}
+import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
+
+export default {
+  name: 'Count',
+  data() {
+    return {
+      n: 1, //用户选择的数字
+    }
+  },
+  methods: {
+    increment() {
+      // this.$store.dispatch('jia',this.n) //因为action中的jia没有做任何附加处理，所以可以直接调用mutations中的JIA处理数据
+      this.$store.commit('JIA', this.n)
+    },
+    decrement() {
+      // this.$store.dispatch('jian',this.n)
+      this.$store.commit('JIAN', this.n)
+    },
+    incrementOdd() {
+      this.$store.dispatch('jiaOdd', this.n)
+    },
+    incrementWait() {
+      this.$store.dispatch('jiaWait', this.n)
+    },
+    //靠mapMutations生成：mapIncrement、mapDecrement（对象形式）
+    ...mapMutations({mapIncrement:'JIA',mapDecrement:'JIAN'}),//k:v > 方法名：mutations中的方法名
+    //...mapMutations(['JIA','JIAN']),//（数组形式）方法名相同才能用
+
+    //靠mapActions生成：mapIncrementOdd、mapIncrementWait（对象形式）
+    ...mapActions({mapIncrementOdd:'jiaOdd',mapIncrementWait:'jiaWait'})
+    //...mapActions(['jiaOdd','jiaWait'])//（数组形式）方法名相同才能用
+  },
+  computed: {
+    //借助mapState生成计算属性，从state中读取数据
+    // ...mapState({school: 'school', subject: 'subject'}),//对象写法
+    ...mapState(['school', 'subject']),//数组写法
+    ...mapGetters({bigSum: 'bigSum'}),//对象写法
+    //...mapGetters({bigSum: 'bigSum'}),//数组写法
+  }
+}
 </script>
 
 <style lang="css">
-	button{
-		margin-left: 5px;
-	}
+button {
+  margin-left: 5px;
+}
 </style>
